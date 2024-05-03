8233623f2f77b922f488281ee40a6fe2b1b7bdb1
ameliajohnson <ameliamj@cs.washington.edu>
2024-05-03 15:02:12 -0700

    w/ birdirectionality

--- model/mrnn.py
+++ model/mrnn.py
@@ -2,27 +2,28 @@ import torch
 from torch import nn
 
 class MilliesRNN(nn.Module):
-    def __init__(self, input_size, hidden_size, output_size):
+    def __init__(self, input_size, hidden_size, output_size, bidirc=False):
         super(MilliesRNN, self).__init__()
         self.input_size = input_size
         self.hidden_size = hidden_size
         self.output_size = output_size
 
-        self.thal_vis = False
-        self.mot_thal = False
+        self.bidirc = bidirc
+        if self.bidirc:
+            self.hidden_size += self.output_size
 
-        # visual cortext
-        self.i2h = nn.Linear(input_size, hidden_size)
-        self.h2h = nn.Linear(hidden_size, hidden_size)
-        self.h2o = nn.Linear(hidden_size, output_size)
+        # visual cortex
+        self.i2h = nn.Linear(self.input_size, self.hidden_size - self.output_size)
+        self.h2h = nn.Linear(self.hidden_size, self.hidden_size)
+        self.h2o = nn.Linear(self.hidden_size, self.output_size)
 
         # thalamus
-        self.thal = nn.Linear(output_size, output_size)
+        self.thal = nn.Linear(2 * self.output_size, self.output_size)
 
         # motor cortex
-        self.i2h_dos = nn.Linear(output_size, hidden_size)
-        self.h2h_dos = nn.Linear(hidden_size, hidden_size)
-        self.h2o_dos = nn.Linear(hidden_size, output_size)
+        self.i2h_dos = nn.Linear(self.output_size, self.hidden_size)
+        self.h2h_dos = nn.Linear(self.hidden_size, self.hidden_size)
+        self.h2o_dos = nn.Linear(self.hidden_size, self.output_size)
         
         
 
@@ -39,22 +40,20 @@ class MilliesRNN(nn.Module):
         outputs_v = torch.zeros((batch_size, trial_len, self.output_size))
         outputs_m = torch.zeros((batch_size, trial_len, self.output_size))
         outputs_t = torch.zeros((batch_size, trial_len, self.output_size))
-        hidden_states_v = torch.zeros((data.shape[0], data.shape[1], self.hidden_size))
-        hidden_states_m = torch.zeros((data.shape[0], data.shape[1], self.hidden_size))
+        hidden_states_v = torch.zeros((batch_size, trial_len, self.hidden_size))
+        hidden_states_m = torch.zeros((batch_size, trial_len, self.hidden_size))
         hidden_state_v = self.init_hidden(batch_size)
         hidden_state_m = self.init_hidden(batch_size)
 
         inp_mots = torch.zeros((batch_size, trial_len, self.output_size))
-
-        torch.autograd.set_detect_anomaly(True)
 
         
         for i in range(trial_len):
             # visual cortex
             # inp_vis = data[:, i, :] # N x L
             inp_vis = self.i2h(data[:, i, :]) # N x H
-            if i > 0 and self.thal_vis:
-                inp_vis = torch.cat((inp_vis, outputs_t[:, i-1,:]))
+            if self.bidirc: 
+                inp_vis = torch.cat((inp_vis, outputs_t[:, i-1,:]), dim=1)
             hidden_state_v = self.h2h(hidden_state_v)
             hidden_state_v = self.retanh(inp_vis + hidden_state_v)
             out_v = self.h2o(hidden_state_v)
@@ -63,8 +62,8 @@ class MilliesRNN(nn.Module):
 
             # thalamus
             inp_thal = out_v
-            if i > 0 and self.mot_thal:
-                inp_thal = torch.cat((inp_thal, outputs_m[:, i-1, :]))
+            if self.bidirc:
+                inp_thal = torch.cat((inp_thal, outputs_m[:, i-1, :]), dim=1)
             out_thal = self.retanh(self.thal(inp_thal))
             outputs_t[:, i, :] = out_thal
 
